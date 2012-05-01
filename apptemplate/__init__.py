35181bcdf0f9b9c4c03da051b14fb0f1338c2d4c
Paul Cooney <p@ulcooney.co.uk>
2012-05-01 15:08:48 +0100

    Handle case when semver cannot be imported

--- apptemplate/__init__.py
+++ apptemplate/__init__.py
@@ -1,13 +1,18 @@
 """
 Example docs about apptemplate module
 """
-import semver as _semver
 import os
 _version_path = os.path.join(os.path.dirname(__file__), 'version.txt')
 
 VERSION = open(_version_path).read().rstrip() #rstrip() removes newlines
-VERSION_INFO = _semver.parse(VERSION)
 AUTHOR = 'some author'
 AUTHOR_EMAIL = 'someemail@invalid.com'
 PROJECT_NAME = os.path.split(os.path.dirname(os.path.abspath(__file__)))[-1]
 SHORT_DESCRIPTION = 'some info about this project'
+
+try:
+    import semver as _semver
+except ImportError:
+    pass
+else:
+    VERSION_INFO = _semver.parse(VERSION)
