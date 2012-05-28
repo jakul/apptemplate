24f96398619d9b4720d6d5caf2e5840ceec75691
Paul Cooney <p@ulcooney.co.uk>
2012-05-28 10:47:09 +0100

    Run tests with failfast turned off

--- runtests.py
+++ runtests.py
@@ -1,20 +1,18 @@
 import os, sys
 
-APPNAME = 'neuron'
+APPNAME = 'apptemplate'
 
 if __name__ == '__main__':
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
+    
     if len(sys.argv) == 1:
         testModules = [APPNAME,]
     else:
         testModules = sys.argv[1:]
 
     from django.test.simple import DjangoTestSuiteRunner
-    runner = DjangoTestSuiteRunner()
-    failures = runner.run_tests(testModules, verbosity=1, interactive=True)
+    runner = DjangoTestSuiteRunner(failfast=False)
+    failures = runner.run_tests(testModules)
 
     sys.exit(failures)
-
-
